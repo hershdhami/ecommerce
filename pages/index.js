9a8d558a242e121b5795524caf590ced471cb739
hershdhami <69739606+hershdhami@users.noreply.github.com>
2022-06-19 20:56:50 -0400

    Added component functionality | Fixed babelrc bug

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,22 @@
 import React from 'react'
+import { Product, FooterBanner, HeroBanner } from '../components';
 
 const Home = () => {
   return (
     <>
-      HeroBanner
+      <HeroBanner />
 
-      <div>
+      <div className='products-heading'>
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
 
-      <div>
+      <div className='products-container'>
         {['Product 1', 'Product 2'].map(
           (product) => product)}
       </div>
 
-      Footer
+      <FooterBanner />
     </>
   )
 }
