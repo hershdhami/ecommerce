58856ceaca554901a1032e99aa449325e9cd9951
hershdhami <69739606+hershdhami@users.noreply.github.com>
2022-06-21 15:19:56 -0400

    Adding a list of products to the homepage

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
 
-const Product = () => {
+import { urlFor } from '../lib/client';
+
+const Product = ({ product: { image, name, slug, price } }) => {
   return (
-    <div>Product</div>
+    <div>
+      <Link href={`/product/${slug.current}`}> 
+      <div className='product-card'>
+        <img 
+          src={urlFor(image && image[0])}
+          width={250}
+          height={250}
+          className="product-image"
+        />
+
+      </div>
+      </Link>
+    </div>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ const Home = ({ products, bannerData }) => {
   return (
     <div>
       <HeroBanner heroBanner={bannerData.length ? bannerData[0] : {}} />
-      {console.log(bannerData)}
+      {console.log(products)}
 
       <div className='products-heading'>
         <h2>Best Selling Products</h2>
