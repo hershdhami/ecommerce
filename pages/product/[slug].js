71b78dde9c52770e02974c35b3fbf9d94894fd68
hershdhami <69739606+hershdhami@users.noreply.github.com>
2022-06-23 11:43:01 -0400

    Added the product details page to the website

---
+++ pages/product/[slug].js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { client, urlFor } from '../../lib/client';
+
+const ProductDetails = ({ product, products }) => {
+    const { image, name, details, price } = product;
+
+    return (
+    <div>
+        <div className='product-detail-container'>
+            <div>
+                <div className='image-container'>
+                    <img src={urlFor(image && image[0])} />
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export const getStaticPaths = async () => {
+    const query = `*[_type == "product"] {
+        slug {
+            current
+        }
+    }
+    `;
+
+    const products = await client.fetch(query)
+
+    const paths = products.map((product) => ({
+        params: {
+            slug: product.slug.current
+        }
+    }))
+
+    return {
+        paths,
+        fallback: 'blocking'
+    }
+}
+
+export const getStaticProps = async ({ params: { slug }}) => {
+    const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
+    const productsQuery = '*[_type == "product"]';
+
+    const product = await client.fetch(query);
+    const products = await client.fetch(productsQuery);
+
+    return {
+      props: { product, products }
+    }
+  }
+
+export default ProductDetails
