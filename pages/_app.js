89be8c0daad474ddc94b23157f3800a056c5004d
hershdhami <69739606+hershdhami@users.noreply.github.com>
2022-06-24 12:00:06 -0400

    Added the product details page to the website

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,16 @@
 import '../styles/globals.css';
 import { Layout } from '../components';
+import { StateContext } from '../context/StateContext';
 import React from 'react';
+import { Toaster } from 'react-hot-toast';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <StateContext>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
   )
 }
 
