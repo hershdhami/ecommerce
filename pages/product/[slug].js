2c658e84a0383a4bb43085a9b6ab4b1afa344188
hershdhami <69739606+hershdhami@users.noreply.github.com>
2022-06-24 11:29:14 -0400

    Added the product details page to the website

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -20,22 +20,36 @@ const ProductDetails = ({ product, products }) => {
                         onMouseEnter="" />
                     ))}
                 </div> */}
-                <div className='product-details-desc'> 
-                    <h1>{name}</h1>
-                    <div className='reviews'>
-                        <div>
-                            <AiFillStar />
-                            <AiFillStar />
-                            <AiFillStar />
-                            <AiFillStar />
-                            <AiOutlineStar />
-                        </div>
-                        <p>
-                            (20)
-                        </p>
+            </div>
+
+            <div className='product-detail-desc'> 
+                <h1>{name}</h1>
+                <div className='reviews'>
+                    <div>
+                        <AiFillStar />
+                        <AiFillStar />
+                        <AiFillStar />
+                        <AiFillStar />
+                        <AiOutlineStar />
                     </div>
-                    <h4>Details: </h4>
-                    <p>{details}</p>
+                    <p>
+                        (20)
+                    </p>
+                </div>
+                <h4>Details: </h4>
+                <p>{details}</p>
+                <p className='price'>${price}</p>
+                <div className='quantity'>
+                    <h3>Quantity</h3>
+                    <p className='quantity-desc'>
+                        <span className='minus' onClick=""><AiOutlineMinus/></span>
+                        <span className='num' onClick="">0</span>
+                        <span className='plus' onClick=""><AiOutlinePlus/></span>
+                    </p>
+                </div>
+                <div className='buttons'>
+                    <button type='button' className='add-to-cart' onClick="">Add to Cart</button>
+                    <button type='button' className='buy-now' onClick="">Buy Now</button>
                 </div>
             </div>
         </div>
