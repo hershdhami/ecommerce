459c3caa6c9040c2e013606c6649629e9b95d417
hershdhami <69739606+hershdhami@users.noreply.github.com>
2022-06-24 11:49:18 -0400

    Added the product details page to the website

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,26 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { client, urlFor } from '../../lib/client';
 import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai';
 import { Product } from '../../components';
 
 const ProductDetails = ({ product, products }) => {
     const { image, name, details, price } = product;
+    const [index, setIndex] = useState(0);
 
     return (
     <div>
         <div className='product-detail-container'>
             <div>
                 <div className='image-container'>
-                    <img src={urlFor(image && image[0])} className='image-card' />
+                    <img src={urlFor(image && image[index])} className='product-detail-image' />
                 </div>
-                {/* <div className='small-images-container'>
+                <div className='small-images-container'>
                     {image?.map((item, i) => (
                         <img 
                         src={urlFor(item)}
-                        className=""
-                        onMouseEnter="" />
+                        className={i === index ? 'small-image selected-image' : 'small-image'}
+                        onMouseEnter={() => setIndex(i)} />
                     ))}
-                </div> */}
+                </div>
             </div>
 
             <div className='product-detail-desc'> 
