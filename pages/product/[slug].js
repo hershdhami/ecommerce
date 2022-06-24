e2bc8d51c6a1a72e2f4252fa6a28c3f19ec6552f
hershdhami <69739606+hershdhami@users.noreply.github.com>
2022-06-24 11:34:55 -0400

    Added the product details page to the website

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { client, urlFor } from '../../lib/client';
 import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai';
+import { Product } from '../../components';
 
 const ProductDetails = ({ product, products }) => {
     const { image, name, details, price } = product;
@@ -50,6 +51,16 @@ const ProductDetails = ({ product, products }) => {
                 <div className='buttons'>
                     <button type='button' className='add-to-cart' onClick="">Add to Cart</button>
                     <button type='button' className='buy-now' onClick="">Buy Now</button>
+                </div>
+            </div>
+        </div>
+        <div className='maylike-products-wrapper'>
+            <h2>You may also like</h2>
+            <div className='marquee'>
+                <div className='maylike-products-container track'>
+                    {products.map((item) => (
+                        <Product key={item._id} product={item} />
+                    ))}
                 </div>
             </div>
         </div>
