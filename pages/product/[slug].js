b97cbdce0ad943e03b2a406e40172ca83220b647
hershdhami <69739606+hershdhami@users.noreply.github.com>
2022-06-26 21:00:59 -0400

    Adding the increase and decrease qty functions

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -50,7 +50,7 @@ const ProductDetails = ({ product, products }) => {
                     <h3>Quantity</h3>
                     <p className='quantity-desc'>
                         <span className='minus' onClick={decQty}><AiOutlineMinus/></span>
-                        <span className='num' onClick="">{qty}</span>
+                        <span className='num'>{qty}</span>
                         <span className='plus' onClick={incQty}><AiOutlinePlus/></span>
                     </p>
                 </div>
