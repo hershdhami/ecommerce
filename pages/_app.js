3171fcb978c4b74ea58f8a796cb1aa24d184d135
hershdhami <69739606+hershdhami@users.noreply.github.com>
2022-06-26 20:08:00 -0400

    Adding the increase and decrease qty functions

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,9 @@
 import '../styles/globals.css';
+import { Toaster } from 'react-hot-toast';
+
 import { Layout } from '../components';
 import { StateContext } from '../context/StateContext';
 import React from 'react';
-import { Toaster } from 'react-hot-toast';
 
 function MyApp({ Component, pageProps }) {
   return (
