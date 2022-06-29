e121a3ac157d9b39853bac77d1d03069e2a50b43
hershdhami <69739606+hershdhami@users.noreply.github.com>
2022-06-28 22:34:20 -0400

    Started working on the cart component

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
+import Link from 'next/link';
+import { AiOutlineMinus, AiOutlinePlus, AiOutlineLeft, AiOutlineShopping } from 'react-icons/ai';
+import { TiDeleteOutline } from 'react-icons/ti';
+import toast from 'react-hot-toast';
+
+import { useStateContext } from '../context/StateContext';
+import { urlFor } from '../lib/client'
 
 const Cart = () => {
+  const cartRef = useRef();
+
   return (
     <div>Cart</div>
   )
