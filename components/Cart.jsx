98c972797d84ee9cd427326f20ee01a47123438e
hershdhami <69739606+hershdhami@users.noreply.github.com>
2022-06-29 22:19:31 -0400

    Continued working on the cart component

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -9,9 +9,18 @@ import { urlFor } from '../lib/client'
 
 const Cart = () => {
   const cartRef = useRef();
+  const { totalPrice, totalQuantities, cartItems, setShowCart } = useStateContext(); 
 
   return (
-    <div>Cart</div>
+    <div className='cart-wrapper' ref={cartRef}>
+        <div className='cart-container'>
+          <button type='button' className='cart-heading' onClick={() => setShowCart(false)}> 
+            <AiOutlineLeft />
+            <span className='heading'>Your Cart</span>
+            <span className='cart-num-items'>({totalQuantities} items)</span>
+          </button>
+        </div>
+    </div>
   )
 }
 
